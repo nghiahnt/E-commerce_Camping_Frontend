231a0defdc31a4dcb0b60446b5a584448ffd6f8f
trongnghia <trongnghia.25602@gmail.com>
2023-11-01 14:37:37 +0700

    Tái cấu trúc thư mục

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,14 +1,12 @@
 import logo from './logo.png';
-import blogThumbnail from './blogThumbnail.png';
 import main from './main.jpg';
-import thumbnail1 from "./thumbnail1.jpg"
+import thumbnail from "./thumbnail.jpg"
 import user from './user.png';
 
 const images = {
     logo: logo,
-    blogThumbnails: blogThumbnail,
     main: main,
-    thumbnail1: thumbnail1,
+    thumbnail: thumbnail,
     user: user
 }
 
